6a2a38080e768d38522ff343b497b3696aa7a261
drilonbajrami <54851175+drilonbajrami@users.noreply.github.com>
2021-05-10 20:40:12 +0200

    Update Commit
    Nothing big added yet, testing out somethings first.

--- Wanderer/Assets/Scripts/Agent.cs
+++ Wanderer/Assets/Scripts/Agent.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Bounds bounds;
+    private NavMeshAgent agent;
+    
+    
+    private void Start()
     {
-        
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        bounds = GameObject.FindGameObjectWithTag("Terrain").GetComponent<MeshCollider>().bounds;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(!agent.hasPath)
+            agent.SetDestination(GetRandomDestination());
+    }
+
+    private Vector3 GetRandomDestination()
+    {
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        return new Vector3(x, y, 0);
     }
 }
